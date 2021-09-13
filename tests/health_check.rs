98b83523dd580e08fb04973ab510c047402e98d8
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2021-09-13 17:22:27 +0200

    formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,7 +17,7 @@ async fn health_check_works() {
 }
 
 #[actix_rt::test]
-async fn subscribe_returns_a_200_for_valid_form_data(){
+async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
@@ -37,14 +37,14 @@ async fn subscribe_returns_a_200_for_valid_form_data(){
 }
 
 #[actix_rt::test]
-async fn subscribe_returns_a_400_when_data_is_missing(){
+async fn subscribe_returns_a_400_when_data_is_missing() {
     // Arrange
     let address = spawn_app();
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=Andrea", "missing the email"),
         ("email=andrea%40dilis.io", "missing the name"),
-        ("", "missing both name and email")
+        ("", "missing both name and email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
@@ -58,8 +58,9 @@ async fn subscribe_returns_a_400_when_data_is_missing(){
             .expect("Failed to execute request.");
 
         // Assert
-        assert_eq!(400,
-                   response.status().as_u16(),
+        assert_eq!(
+            400,
+            response.status().as_u16(),
             "The API did not fail with 400 Bad Request when the payload was {}",
             error_message
         )
@@ -67,8 +68,7 @@ async fn subscribe_returns_a_400_when_data_is_missing(){
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("failed to bind address");
     let _ = tokio::spawn(server);
