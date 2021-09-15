39ff67c3310647ac7b1e9904de08c5e722c4e3fb
Andrea Di Lisio <andrea.dilisio91@gmail.com>
2021-09-15 18:13:13 +0200

    env variable file to load a db url

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,7 +24,8 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let address = spawn_app();
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_string = configuration.database.connection_string();
-    let _connection = PgConnection::connect(&connection_string)
+    // mutable is required for below assertions
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres");
 
@@ -41,7 +42,15 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     // Assert
-    assert_eq!(200, response.status().as_u16())
+    assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.name, "Andrea%20Di%20Lisio");
+    assert_eq!(saved.email, "andrea%40dilis.io");
 }
 
 #[actix_rt::test]
